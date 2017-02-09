cd73f19ce561561bf3309b18162d49dfc87e4045
mccaffpm <mccaffpm@gmail.com>
2017-02-09 13:53:25 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1uo40yPkOwMtdVrEN76am8IkcGeOOuPo9721vDcoqLAk/pubhtml?gid=0&single=true';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/12XYB-d4FqKpSDX8gmQMDkgj107zoctTBc1-H1Icmzls/pubhtml';
